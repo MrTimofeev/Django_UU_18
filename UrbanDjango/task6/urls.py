fa84be2c2a37dffa0037d84569eefd81a7ecd63b
MrTimofeev <nemo.capitan.26ru@mail.ru>
2024-12-02 17:48:22 +0300

    88. Pagination (Completed)

--- UrbanDjango/task6/urls.py
+++ UrbanDjango/task6/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view, shop_view, cart_view, sign_up_by_django, sign_up_by_html, create_records, clear_database
+from .views import *
 
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('sign_up_html/', sign_up_by_html, name='sign_up_html'),
     path('create_records/', create_records, name='create_records'),
     path('clear_database/', clear_database, name='clear_database'),
+    path('view_objects_with_pagination/', view_objects_with_pagination, name='view_objects_with_pagination'),
 ]
